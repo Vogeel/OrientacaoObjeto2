6a07114af38c7c660df94c9c33f4f106a10b7f61
Vogeel <vitorvogel12@gmail.com>
2022-03-08 15:01:31 -0300

    Add project files.

---
+++ OrientacaoObjeto2.ConsoleApp/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrientacaoObjeto2.ConsoleApp
+{
+    internal class GrausFahrenheit
+    {
+        public double F;
+        
+
+        public double Convertido()
+        {
+            double ValorFinal = (F - 32) / 1.8;
+            return ValorFinal;
+        }
+    }
+}
---
+++ OrientacaoObjeto2.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OrientacaoObjeto2.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GrausFahrenheit conversao = new GrausFahrenheit();
+            conversao.F = 20;
+            Console.WriteLine($"{conversao.F}° Em Fahrenheit é a mesma coisa que {conversao.Convertido():F2}° graus Celcius");
+            Console.ReadKey();
+        }
+    }
+}
